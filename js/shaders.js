0c5e54e1f0815909d4bc97670c3de2f2d5f02029
iniadewumi <ini.adewumi@gmail.com>
2025-06-07 12:04:17 -0500

    shader adjustments

--- js/shaders.js
+++ js/shaders.js
@@ -226,7 +226,7 @@ float getBass() {
     for (int i = 0; i < 5; i++) {
         bass += texture(iChannel0, vec2(0.01 + 0.02*float(i), 0.0)).x;
     }
-    bass /= 5.0;
+    bass /= 30.0;
     
     // Dampen to avoid extreme reactions
     return 0.4 + 0.3 * smoothstep(0.0, 0.7, bass);
@@ -945,106 +945,6 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     // Apply gamma correction
     fragColor.rgb = sqrt(max(col, vec3(0.001))); // Prevent negative values
     fragColor.a = 1.0;
-}`,
-"Muah": ` #define MARCHLIMIT 70
-
-vec3 camPos = vec3(0.0, 0.0, -1.0);
-vec3 ld = vec3(0.0, 0.0, 1.0);
-vec3 up = vec3(0.0, 1.0, 0.0);
-vec3 right = vec3(1.0, 0.0, 0.0);
-vec3 lightpos = vec3(1.5, 1.5, 1.5);
-
-
-// Smooth HSV to RGB conversion 
-vec3 hsv2rgb_smooth( in vec3 c )
-{
-    vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
-
-	rgb = rgb*rgb*(3.0-2.0*rgb); // cubic smoothing	
-
-	return c.z * mix( vec3(1.0), rgb, c.y);
-}
-
-vec4 range(vec3 p)
-{
-
-    // Sphere with Radius
-    vec3 spherepos = vec3(0.0, 0.0, 0.0);
-    float radius = log(sin(iTime*0.1)*0.05+1.0)+0.1;
-	
-    //float anim = floor(cos(iTime*0.4)+1.0);
-    float anim = smoothstep(0., .1, cos(iTime*0.4)+1.0);
-    
-    //float anim2 = floor(-cos(iTime*0.4)+1.0);
-    float anim2 = smoothstep(0., .1, -cos(iTime*0.4)+1.0);
-    
-    float xampl = sin(iTime*1.3)*0.4*anim;
-    float yampl = sin(iTime*1.3)*0.4-(anim2*0.3);
-    
-    p.x += cos((max(-2.0+p.z-camPos.z,0.)))*xampl-xampl;
-    p.y += sin((max(-2.0+p.z-camPos.z,0.)))*yampl;
-    
-    
-    p = mod(p + vec3(0.5,0.5,0.5), vec3(1.0,1.0,1.0)) - vec3(0.5,0.5,0.5);
-    spherepos = mod(spherepos + vec3(0.5,0.5,0.5), vec3(1.0,1.0,1.0)) - vec3(0.5,0.5,0.5);
-    
-    vec3 diff = p - spherepos;
-    
-    vec3 normal = normalize(diff);
-
-    
-    return vec4(normal, length(diff)-radius);
-}
-
-vec3 lerp(vec3 a, vec3 b, float p)
-{
-    p = clamp(p,0.,1.);
- 	return a*(1.0-p)+b*p;   
-}
-
-
-vec4 march(vec3 cam, vec3 n)
-{
-    
-    float len = 1.0;
-    vec4 ret;
-    
-    for(int i = 0; i < MARCHLIMIT; i++)
-    {
-        ret = range(camPos + len*n)*0.5;
-		len += ret.w;
-    }
-    
-	return vec4(ret.xyz, len);
-}
-
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    float colorangle = 0.;
-    
-	vec2 uv = (fragCoord.xy*2.0) / iResolution.xy - vec2(1, 1);
-    uv.x *= iResolution.x / iResolution.y;
-    
-    float rotangle = iTime*0.08;
-    vec2 newuv;
-    newuv.x = uv.x*cos(rotangle)-uv.y*sin(rotangle);
-    newuv.y = uv.x*sin(rotangle)+uv.y*cos(rotangle);
-    uv = newuv;
-    
-    camPos = vec3(0.5, 0.5, iTime*1.0);
-
-    //ld = normalize(vec3(0.0, sin(iTime*0.8)*0.1, cos(iTime*0.8)*0.5));
-    float zoom = 0.6;
-    vec3 n = normalize(vec3(sin(uv.x*3.1415*zoom),sin(uv.y*3.1415*zoom) ,ld.z*cos(uv.x*3.1415*zoom)*cos(uv.y*3.1415*zoom)));
-    vec4 rangeret = march(camPos, n);
-    float d = log(rangeret.w / 1.0 + 1.0);
-    vec3 normal = rangeret.xyz;
-    
-    vec3 p = camPos + n*d;
-    float angle = acos(dot(normal, n)/length(normal)*length(n));
-    
-	fragColor = vec4(hsv2rgb_smooth(lerp(vec3(d*0.1 + (colorangle + iTime)*0.01 + atan(uv.y/uv.x)*3.1415 , 2.0, max(1.0 - log(d),0.0)),vec3(d*0.1 + ((colorangle + iTime)+120.0)*0.01 , 2.0, max(1.0 - log(d),0.0)),cos(angle/10.0))),1.0);
 }`,
 "Muah Sound": `float getAudioReactivity() {
     // Sample multiple frequency ranges
